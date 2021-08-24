406adefd14eb4f77bd569f735369c7859fbbcce4
Henry Cagnini <henry.cagnini@gmail.com>
2021-08-24 19:24:10 -0300

    removed instructions for logging metadata as it was causing errors on linux

--- autocve_procedure.py
+++ autocve_procedure.py
@@ -105,12 +105,7 @@ def run_holdout(
 
         combination = get_pbil_combination()  # type: dict
 
-        good = True
-        try:
-            os.mkdir(os.path.join(metadata_path, experiment_folder, dataset_name, 'sample_%02d_fold_00' % n_trial))
-        except:
-            good = False
-
+        # os.mkdir(os.path.join(metadata_path, experiment_folder, dataset_name, 'sample_%02d_fold_00' % n_trial))
 
         pbil = PBIL(
             resources_path=os.path.join(sys.modules['mPBIL'].__path__[0], 'resources'),
@@ -120,17 +115,13 @@ def run_holdout(
             n_individuals=combination['n_individuals'],
             timeout=combination['timeout'], timeout_individual=combination['timeout_individual'],
             n_folds=combination['n_folds'], fitness_metric=combination['fitness_metric'],
-            log_path=os.path.join(metadata_path, experiment_folder, dataset_name, 'sample_%02d_fold_00' % n_trial) if good else None
+            # log_path=os.path.join(metadata_path, experiment_folder, dataset_name, 'sample_%02d_fold_00' % n_trial) if good else None
         )
 
         _, clf = pbil.run(1)
 
-        if good:
-            try:
-                pbil.logger.individual_to_file(individual=clf, individual_name='last', step=pbil.n_generation)
-                pbil.logger.probabilities_to_file()
-            except:
-                pass
+        # pbil.logger.individual_to_file(individual=clf, individual_name='last', step=pbil.n_generation)
+        # pbil.logger.probabilities_to_file()
 
         external_preds = list(map(list, clf.predict_proba(test_data)))
         external_actual_classes = list(test_data.values(test_data.class_index).astype(np.int))
